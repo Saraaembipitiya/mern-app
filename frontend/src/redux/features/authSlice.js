210957346c7b16641f580025c76f9e6bb526e319
Saraa <sathsarasenanayaka96@gmail.com>
2022-04-22 11:46:18 +0530

    frontend add

---
+++ frontend/src/redux/features/authSlice.js
@@ -0,0 +1,105 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import * as api from "../api";
+
+export const googleSignIn = createAsyncThunk(
+  "auth/googleLogin",
+  async ({ profileObj, navigate, toast }, { rejectWithValue }) => {
+    try {
+      const response = await api.googleSignIn(profileObj);
+      toast.success("Successfully logged in");
+      navigate("/");
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ formValue, navigate, toast }, { rejectWithValue }) => {
+    try {
+      const response = await api.signIn(formValue);
+      toast.success("Login successfully");
+      navigate("/");
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({ formValue, navigate, toast }, { rejectWithValue }) => {
+    try {
+      const response = await api.signUp(formValue);
+      toast.success("Register successfully");
+      navigate("/");
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+//create slice
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    error: "",
+    loading: false,
+  },
+  extraReducers: {
+    [login.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [login.fulfilled]: (state, action) => {
+      console.log("wroking");
+      state.loading = false;
+      localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
+      console.log(action);
+      state.user = action.payload;
+    },
+    [login.rejected]: (state, action) => {
+      state.loading = false;
+      console.log(action.payload);
+
+      state.error = action.payload;
+    },
+    [register.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [register.fulfilled]: (state, action) => {
+      console.log("wroking");
+      state.loading = false;
+      localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
+      console.log(action);
+      state.user = action.payload;
+    },
+    [register.rejected]: (state, action) => {
+      state.loading = false;
+      console.log(action.payload);
+
+      state.error = action.payload;
+    },
+    [googleSignIn.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [googleSignIn.fulfilled]: (state, action) => {
+      console.log("wroking");
+      state.loading = false;
+      localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
+      console.log(action);
+      state.user = action.payload;
+    },
+    [googleSignIn.rejected]: (state, action) => {
+      state.loading = false;
+      console.log(action.payload);
+
+      state.error = action.payload;
+    },
+  },
+});
+
+export default authSlice.reducer;
